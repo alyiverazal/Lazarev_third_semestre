4ec962f059fada54281cbff34bad06070389b4e3
alyiverazal <ilya-lazarev3012@yandex.ru>
2021-09-14 23:05:24 +0300

    Beta1
    Добавлена менюшка, но её функционал пока не реализован

--- Lab2-4/Lab2-4/Lab2-4.cpp
+++ Lab2-4/Lab2-4/Lab2-4.cpp
@@ -4,31 +4,36 @@
 #include <iostream>
 #include "Lab2-4.h"
 
-void OutputPipe(Pipe& My_Pipe)
-{
-    std::cout << "pipe's id" << My_Pipe.id;
-    std::cout << "pipe's length " << My_Pipe.length << "\n";
-    std::cout << "pipe's width " << My_Pipe.width << "\n";
-    std::cout << "Pipes under repair: " << My_Pipe.under_repair;
-}
+//std::cout << "" << "\n";
+//std::cin >> ;
+
+
 
 void InputPipe(Pipe& My_Pipe)
 {
-    std::cout << "Enter what length of pipe do you want:";
+    std::cout << "Enter what length of pipe do you want:" << "\n";
     std::cin >> My_Pipe.length;
-    std::cout << "Enter what width of pipe do you want:";
+    std::cout << "Enter what width of pipe do you want:" << "\n";
     std::cin >> My_Pipe.width;
+}
+
+void OutputPipe(Pipe& My_Pipe)
+{
+    std::cout << "pipe's id" << My_Pipe.id << "\n";
+    std::cout << "pipe's length " << My_Pipe.length << "\n";
+    std::cout << "pipe's width " << My_Pipe.width << "\n";
+    std::cout << "Pipes under repair: " << My_Pipe.under_repair << "\n";
 }
 
 void InputStation(Comp_Station& Station)
 {
-    std::cout << "Enter what name of Station do you want:";
+    std::cout << "Enter what name of Station do you want:" << "\n";
     std::cin >> Station.name;
-    std::cout << "Enter what number_of_workshops of Station do you want:";
+    std::cout << "Enter what number_of_workshops of Station do you want:" << "\n";
     std::cin >> Station.number_of_workshops;
-    std::cout << "Enter what working_workshops of Station do you want:";
+    std::cout << "Enter what working_workshops of Station do you want:" << "\n";
     std::cin >> Station.working_workshops;
-    std::cout << "Enter what performance of Station do you want:";
+    std::cout << "Enter what performance of Station do you want:" << "\n";
     std::cin >> Station.performance;
 }
 
@@ -38,7 +43,7 @@ void OutputStation(Comp_Station& Station)
     std::cout << "station's name: " << Station.name << "\n";
     std::cout << "station's number of workshops: " << Station.number_of_workshops << "\n";
     std::cout << "station's performance " << Station.performance << "\n";
-    std::cout << "station's working workshops " << Station.working_workshops;
+    std::cout << "station's working workshops " << Station.working_workshops << "\n";
 }
 
 int main()
@@ -52,9 +57,30 @@ int main()
     OutputPipe(My_Pipe);
     InputStation(Station);
     OutputStation(Station);
+    std::string InputW;
+    int i = 1;
+    
+    while (i = 1) { //если поставить == при входе в цикл компилятор начнет бесконечно выводить первую строку
+        std::cout << "Pirnt 'AddPipe' if you want to add pipe" << "\n";
+        std::cout << "Print 'AddCompStation' if you want to add Compression station" << "\n";
+        std::cout << "Print 'VAO' if you want to see all objects" << "\n";
+        std::cout << "Print 'ChangePipe' if you want to change pipe" << "\n";
+        std::cout << "" << "\n";
+        std::cout << "" << "\n";
+        std::cout << "" << "\n";
+        std::cin >> InputW;
+        if (InputW == "AddPipe") {
 
+        }
+
+
+
+        if (InputW == "Exit") break;
+    }
+    return 0;
 
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
